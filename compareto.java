77a2be2bee69b9e67e71a5657c6c2f95a4b6e132
Prarthana-git <prarthanachaudhari@gmail.com>
2021-05-24 22:44:50 +0530

    Used campareTo function

---
+++ compareto.java
@@ -0,0 +1,33 @@
+public class compareto
+
+{
+    public static void main(String args[])
+   {
+        int x1=3;
+        int x2=7;
+        int y1=4;
+        int y2=5;
+     
+     double length1=Math.sqrt((x2-x1)*(x2-x1));
+     double length2=Math.sqrt((y2-y1)*(y2-y1));
+
+     String obj1=String.valueOf(length1);      
+     String obj2=String.valueOf(length2);
+    
+     double result=obj1.compareTo(obj2);
+		
+		 
+     if(result == 0)
+   {
+       System.out.println("Both line are same");
+   }
+    else if (result > 0 )
+  {
+      System.out.println("Line 1 is bigger than Line 2");
+   }
+   else
+  {
+    System.out.println("Line 2 is bigger than Line 1");
+  }
+     }
+} 
