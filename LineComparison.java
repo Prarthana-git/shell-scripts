9b996ba71fd7e96fe6ef76b11c104dfa933a49f4
Prarthana-git <prarthanachaudhari@gmail.com>
2021-05-24 19:22:02 +0530

    Welcome to Line Camparision Computation

---
+++ LineComparison.java
@@ -0,0 +1,19 @@
+public class LineComparison
+ {
+
+	public static void main(String[] args) 
+	{
+		int x1=3;
+		int y1=4;
+		int x2=7;
+		int y2=1;
+		
+		public static void main(String[] args) 
+	{
+		System.out.println(" Welcome to Line Comparison Computation Program");
+                System.out.println("************************************");
+
+	
+		
+	}
+}
